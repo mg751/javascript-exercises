d5feb3e5148456dbddcb6a62f8bab79e529d549b
mg751 <mgoreansky@gmail.com>
2022-07-24 13:54:15 +0300

    Complete 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string="default", num) {
+    let output
+    repetitions = parseInt(num);
+    if (num > 0) {
+        output=string;
+        for (i = 1; i < repetitions; i++) {
+            output += string;
+            }
+        } else if (num < 0) {
+            output="ERROR"
+        } else {
+            output=""; 
+    }
+    return(output);
+}
 
 // Do not edit below this line
 module.exports = repeatString;
