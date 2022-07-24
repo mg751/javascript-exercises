d74c152b35d1dee93f2f5da15bb51ac6d1b09fa3
mg751 <mgoreansky@gmail.com>
2022-07-24 16:01:56 +0300

    Create array input, stuck on filter part

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, filter) {
+    let inputArray = Array.from(array);
+    let stringArray = inputArray.map(String);
+    filterString = toString(filter);
 
-};
+    function checkArray(string){
+     return (string !== filterString);
+    
+    }
 
+    let output = stringArray.filter(checkArray);
+    return(output);
+        
+}
 // Do not edit below this line
 module.exports = removeFromArray;
