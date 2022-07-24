3155a07ee7fe728bca9d9cafc2cf07109a1da283
mg751 <mgoreansky@gmail.com>
2022-07-24 14:49:00 +0300

    Solve riddle

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let splittedString = string.split("");
+    let stringLength = splittedString.length;
+    let negStringLength = Math.abs(stringLength) * -1;
+    let output ="";
+    for (let i = -1; i >= negStringLength; i--) {
+        output += splittedString[splittedString.length + i];
+    }
+    return(output);
 
 };
 
