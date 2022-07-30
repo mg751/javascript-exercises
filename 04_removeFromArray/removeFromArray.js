96b3edc6a7df762b1c9619c72d64b6cecd446f91
mg751 <mgoreansky@gmail.com>
2022-07-30 12:07:17 +0300

    Create basic function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,16 +1,29 @@
-const removeFromArray = function(array, filter) {
-    let inputArray = Array.from(array);
-    let stringArray = inputArray.map(String);
-    filterString = toString(filter);
+let removeFromArray = function(inputArray,inputElement){
 
-    function checkArray(string){
-     return (string !== filterString);
+
+
     
+    let equalityTest = function checkEquality(inputArray){
+        return (inputElement !== inputArray)
     }
 
-    let output = stringArray.filter(checkArray);
-    return(output);
-        
+    newArray = inputArray.filter(equalityTest);
+        return(newArray);
 }
+
+
+
+
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+// Psuedocode
+// Create test array
+// Create function to test input equality
+// to element in array
+// Take specific element from test array out, 
+// assign new array without the said element
+// Create function to remove specific argument from array
+// 
+//
