54dd5dc46fa0d9f0efe8bb907be2c476b3475093
mg751 <mgoreansky@gmail.com>
2022-07-30 15:21:20 +0300

    Pass first test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a,b){
+    let array = [];
+    for (let i = a; i <= b; i++){
+        array.push(i);
+    }
+ 
+    let finalSum = 0;
+    for (let i = 0; i <= array.length-1; i++) {
+        finalSum += array[i];
+    }
+    return finalSum;
+}
 
-};
+
 
 // Do not edit below this line
 module.exports = sumAll;
+
