32a215fabd4ba156744717d5ee7c9f1f95bbc3ad
mg751 <mgoreansky@gmail.com>
2022-08-06 11:17:04 +0300

    Complete solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let c = (f-32) * (5/9);
+  console.log(c);
+  let roundedC = Math.round(c * 10) / 10;
+  console.log(roundedC);
+  return roundedC; 
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let f = (c*(9/5)) + 32;
+  let roundedF = Math.round(f * 10) / 10;
+  return roundedF;
 };
 
 // Do not edit below this line
@@ -11,3 +17,4 @@ module.exports = {
   ftoc,
   ctof
 };
+
