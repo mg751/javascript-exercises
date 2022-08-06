e33660445db441263365e313bccb39ddb1d55ab4
mg751 <mgoreansky@gmail.com>
2022-08-06 10:32:12 +0300

    Finish solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
+const leapYears = function(a) {
+    let inputYear = a;
 
-};
+    if (a % 400 === 0) {
+        return true;
+    } else if (a % 4 === 0 && a % 100 !== 0) {
+        return true;
+    } else { 
+        return false;
+    }
 
+}
 // Do not edit below this line
 module.exports = leapYears;
+
+
+// Define function and inputs
+// 
+
