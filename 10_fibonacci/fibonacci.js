583c5151f4795ffc3dc81b20eca8d92947e3ed06
mg751 <mgoreansky@gmail.com>
2022-09-11 15:54:04 +0300

    Finish solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+
+    let input = num;
+    if (num < 0) {
+        return "OOPS";
+    }
+    
+    let array = [1,1];
+    for (let i = 1; i <= input+1; i++) {
+         let sum = array[i] + array[i-1];
+          array.push(sum);
+    }
+
+    return array[num-1];
 
 };
 
