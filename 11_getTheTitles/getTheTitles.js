9c37c726be360a77f4372845085ed92ecdc2d2b3
mg751 <mgoreansky@gmail.com>
2022-09-11 16:18:51 +0300

    Finish exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(obj) {
+    let array = [];
+    for (let i = 0; i < obj.length; i++) {
+        let bookName = obj[i].title;
+        array.push(bookName);
+    }
+return array;
 };
 
 // Do not edit below this line
