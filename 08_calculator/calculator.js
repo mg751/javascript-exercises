cd9ee6472c3645e6f6641a9c4eb563b11ac4f1de
mg751 <mgoreansky@gmail.com>
2022-09-11 12:58:26 +0300

    Finish exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array1) {
+      let total = 0;
+      getSum = (value, total) => total += value;
+      return array1.reduce(getSum, 0);
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let sum = 1;
+  for (let i = 0; i < array.length; i++) {
+      sum = sum * array[i];
+  } 
+  return sum;
+}
 
+const power = function(num, power) {
+  let sum = 1;
+  for (let i = 0; i < power; i++) {
+      sum = sum * num;
+  } 
+  return sum;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+	let array = [];
+  let sum = 1;
+  for (num; num >= 1; num--){
+    array.push(`${num}`);
+  }
+  for (let i = array.length-1; i >= 0; i--) {
+      sum = sum * array[i];
+      console.log(sum);
+  } 
+  return sum;
 };
 
 // Do not edit below this line
