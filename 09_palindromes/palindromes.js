4bc1808d4e57fb5399996fa911ed173e372744af
mg751 <mgoreansky@gmail.com>
2022-09-11 13:11:30 +0300

    Add pseudocode

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,29 @@
 const palindromes = function () {
 
+
+
 };
+
+
+
+// // pseudocode
+// remove spaces and punctuation from input string, make lowercase
+// turn input string into array X, each character sep. by comma
+// define new empty array Y
+// push last value of array X into new array Y
+// test equality of arrays (return true if equal)
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
